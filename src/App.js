905483977e802d8f03ad619199ee0c22a58feae5
IanShih2003 <54551203+IanShih2003@users.noreply.github.com>
2020-08-27 18:49:12 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,119 +1,159 @@
-import React,{useState} from 'react';
-import axios from 'axios';
-import './App.css';
-import { NavLink, BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
-import principal from './principal';
-import { Link } from 'react-router-dom';
-import registrarse from './registrarse';
-import index from './index';
-import style from './style.css';
-import inicio from './inicio';
-import turnos from './turnos';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import {
+  NavLink,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+import principal from "./principal";
+import { Link } from "react-router-dom";
+import registrarse from "./registrarse";
+import index from "./index";
+import style from "./style.css";
+import inicio from "./inicio";
+import turnos from "./turnos";
 
-import { register } from './serviceWorker';
-import prueba from './prueba';
+import { register } from "./serviceWorker";
+import prueba from "./prueba";
 
+function App() {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState('');
+  const [correo, setEmail] = useState("");
+  const [password, setPass] = useState('');
+  const [dni, setDNI] = useState("");
 
- function handleChange(e){
-   console.log(e.target.value);
- }
+  function handleNombre(e) {
+    setNombre(e.target.value)
+    console.log(nombre)   
+  }
+  function handleApellido(e) {
+    setApellido(e.target.value)
+  }
+  function handleCorreo(e) {
+    setEmail(e.target.value)
+  }
+  function handlePass(e) {
+    setPass(e.target.value)
+  }
+  function handleDNI(e) {
+    setDNI(e.target.value)
+  }  
 
+  const Registrousuario = async () => {
+    console.log("hola");
+    const respuesta = await axios(configuracion);
+    console.log(respuesta.data)
+    if (respuesta.data.status === "Employeed Saved") {
+      console.log("funciona");
+    }
+  };
 
+  const configuracion = {
+    url: "http://54.162.46.22:3000/",
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    data: {
+      Nombre: nombre,
+      Apellido: apellido,
+      Correo: correo,
+      password: password,
+      DNI: dni,
+    },
+  };
 
-const configuracion=
-{
-  url:"http://54.162.46.22:3000/",
-  method: "POST",
-  headers:{
-    
-    Accept:"application/json",
-    "Content-Type":"application/json",
-  },data:{Nombre:"pepe",Apellido:"pepe", Correo:"pepe", password:22,DNI:22 }
- 
- }
- const Registrousuario=async()=>{
+  const Login = () => (
+    <div className="contenedorgigante">
+      <div className="header"></div>
+      <div className="cajitafeliz2">
+        <div className="formcontainer">
+          <div className="App"></div>
+  
+          <div style={{ textAlign: "center" }}>
+            <div id="formulario">
+              <input
+                id="nombre"
+                placeholder="Nombre"
+                onChange={e => handleNombre(e)}
+                className="queso"
+                type="text"
+                value ={nombre}
+              />
+            </div>
+            <div>
+              <input
+                id="apellido"
+                placeholder="apellido"
+                onChange={e => handleApellido(e)}
+                className="queso"
+                type="text"
+                value ={apellido}
+              />
+            </div>
+  
+            <div>
+              <input
+                id="correo"
+                placeholder="Email"
+                onChange={e => handleCorreo(e)}
+                className="queso"
+                type="text"
+                value = {correo}
+              />
+            </div>
+            <div>
+              <input
+                id="contra"
+                placeholder="Contraseña"
+                onChange={e => handlePass(e)}
+                className="queso"
+                type="text"
+                value = {password}
+              />
+            </div>
+            <div>
+              <input
+                id="dni"
+                placeholder="DNI"
+                onChange={e => handleDNI(e)}
+                className="queso"
+                type="text"
+                value = {dni}
+              />
+            </div>
+            <div className="xdrow2">
+              <button className="btnlogin" onClick={Registrousuario}>
+                {" "}
+                Registrarse
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
-
-   
-   console.log("hola")
- const respuesta= await axios(configuracion) 
- console.log(respuesta.data)
-
-if(respuesta.data.status === "Employeed Saved"){
- console.log("funciona");
- 
-}
- }  
-
-function App(){
- // const [email, setemail] = useState("");
-  // const onChangeEmail = (e) => { 
- // console.log(e.target.value);
-// setemail(e.target.value) ;
-//} 
-const [email, setemail] = useState('');
-  let history = useHistory();
-  const [firstName, setFirstName] = useState('');
-
-  return(
-    
+  return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/principal" exact component={principal}/>
-          <Route path="/Login" exact component={Login}/>
-          <Route path="/registrarse" exact component={registrarse}/>
-          <Route path="/" exact component={inicio}/>
-          <Route path="/turnos" exact component={turnos}/>
-
-    
+          <Route path="/principal" exact component={principal} />
+          <Route path="/Login" exact component={Login} />
+          <Route path="/registrarse" exact component={registrarse} />
+          <Route path="/" exact component={inicio} />
+          <Route path="/turnos" exact component={turnos} />
         </Switch>
       </div>
     </Router>
   );
 }
 
-   const Login = () => (
-     <div className="contenedorgigante">
-             <div className="header"></div>
-       <div className="cajitafeliz2">
-       <div className="formcontainer">
-         <div className="App"></div>
-     
-<div style={{textAlign: 'center'}} >
-    <div id="formulario">
-        <input id="email" /*onChange={(e => onChangeEmail(e))}*/ placeholder="Nombre" className="queso"type="text"/>
-           
-    </div>
-    <div>
 
-        <input id="password" placeholder="Direccion" className="queso" type="text"/>
-    </div>
-    
-    <div>
- 
-        <input id="apellido"  placeholder="Email" className="queso" type="text"/>
-    </div>
-    <div>
-   
-        <input id="celular" placeholder="Celular" className="queso" type="text"/>
-    </div>
-    <div>
-  
-   
-   <input id="celular" placeholder="Contraseña" className="queso" type="text"/>
-</div>
-<div className="xdrow2">
-    <button className="btnlogin" onClick={Registrousuario} > Registrarse</button>
-    </div>
-    </div>
-    
-
-    </div>
-    </div>
-    
-</div>
-
-    );
 
 export default App;
